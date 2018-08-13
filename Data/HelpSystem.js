a11ab4700622f9ba590b487966d5e07afdd0a493
M. Paquette <marcpaq@gmail.com>
2018-08-12 21:38:38 -0400

    First commit.

---
+++ Data/HelpSystem.js
@@ -0,0 +1,10 @@
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add(
+	'HelpSystem',
+	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
+	'<WebHelpSystem DefaultUrl=\"Content/Topics/Cover.html\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" SearchDatabase=\"Data/Search.xml\" Synonyms=\"Data/Synonyms.xml\" SkinName=\"HTML5\" Skins=\"HTML5\" BuildTime=\"2/16/2017 2:58:24 PM\" BuildVersion=\"8.1.2.27981\" TargetType=\"WebHelp2\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"false\" MakeFileLowerCase=\"false\" UseCustomTopicFileExtension=\"true\" CustomTopicFileExtension=\"html\">' +
+	'    <CatapultSkin Version=\"1\" SkinType=\"WebHelp2\" Comment=\"HTML5 skin\" Anchors=\"Width,Height\" Width=\"800\" Height=\"600\" Top=\"0\" Left=\"0\" Bottom=\"0\" Right=\"0\" Tabs=\"TOC\" DefaultTab=\"TOC\" UseBrowserDefaultSize=\"True\" UseDefaultBrowserSetup=\"True\" DisplayNotificationOptions=\"false\" AutoSyncTOC=\"true\" NavigationLinkTop=\"false\" NavigationLinkBottom=\"false\" Title=\"FlareLint 1.2 User Guide\" Name=\"HTML5\">' +
+	'        <Toolbar EnableCustomLayout=\"true\" Buttons=\"PreviousTopic|NextTopic\" />' +
+	'        <WebHelpOptions HideNavigationOnStartup=\"false\" NavigationPanePosition=\"Left\" />' +
+	'    </CatapultSkin>' +
+	'</WebHelpSystem>'
+);
---
+++ Data/Toc.js
@@ -0,0 +1,25 @@
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add(
+	'Toc',
+	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
+	'<CatapultToc Version=\"1\" DescendantCount=\"18\">' +
+	'    <TocEntry Title=\"FlareLint 1.2 User Guide\" Link=\"/Content/Topics/Cover.html\" ComputedResetPageLayout=\"true\" ComputedFirstTopic=\"true\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"What\'s New\" Link=\"/Content/Topics/WhatsNew.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"About\" Link=\"/Content/Topics/About.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\" FlareLint and MadCap Analyzer\" Link=\"/Content/Topics/VersusAnalyzer.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"System Requirements\" Link=\"/Content/Topics/SystemRequirements.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Installing\" Link=\"/Content/Topics/Installing.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Using\" Link=\"/Content/Topics/Using.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Reading a Report\" Link=\"/Content/Topics/ReadingReport.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Upgrading\" Link=\"/Content/Topics/Upgrading.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Uninstalling\" Link=\"/Content/Topics/Uninstalling.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Sending Feedback\" Link=\"/Content/Topics/Contributing.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Managing Rules\" Link=\"/Content/Topics/Rules.html\" ComputedFirstTopic=\"false\" DescendantCount=\"3\">' +
+	'        <TocEntry Title=\"Ignoring Rules\" Link=\"/Content/Topics/RulesIgnoring.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'        <TocEntry Title=\"Adding Rules\" Link=\"/Content/Topics/RulesAdding.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'        <TocEntry Title=\"Defining a Standard Set of Rules\" Link=\"/Content/Topics/StandardRules.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    </TocEntry>' +
+	'    <TocEntry Title=\"Customizing and Distributing\" Link=\"/Content/Topics/Customizing.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Implementing and Modifying Rules\" Link=\"/Content/Topics/RulesCreating.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'    <TocEntry Title=\"Credits\" Link=\"/Content/Topics/Credits.html\" ComputedFirstTopic=\"false\" DescendantCount=\"0\" />' +
+	'</CatapultToc>'
+);
